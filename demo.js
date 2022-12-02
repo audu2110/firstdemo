cdd4b3c9e15e5b6a02b256021c7f6826474da58c
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-03 00:40:13 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,16 @@
 console.log('I love git');
+
+console.log(document.URL);
+document.title=123;
+console.log(document.head);
+console.log(document.domain);
+console.log(document.doctype);
+console.log(document.getElementById("header-title"));
+
+
+In textcontent doesn't pay attention to styling but the innertext does.
+var headertitle= document.getElementById("header-title"); 
+headertitle.style.backgroundColor='black';
+var add=document.getElementById("add");
+add.innerHTML=<b>"Add Items"</b>
+add.style.color='green';
