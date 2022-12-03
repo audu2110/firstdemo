93b2b920c9f3056ff95db583a4b83d0375f13d30
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-04 00:17:45 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -29,3 +29,22 @@ items[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent="Hello 2";
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
+}
+
+li[2].style.backgroundColor='green';
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
+}
+
+var wli=document.getElementsByClassName('without-same-class');
+wli[0].textContent="Hello 2";
