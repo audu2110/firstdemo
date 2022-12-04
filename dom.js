b1f10266d371410640ee205b258d31882b26e0db
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-04 18:41:51 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -125,3 +125,70 @@ newli.id='hello1';
 var newlitext=document.createTextNode('Hello world');
 newli.appendChild(newlitext);
 console.log(newli);
+
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// edit event
+itemList.addEventListener('click', editItem);
+
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+  var editBtn=document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  editBtn.classname='btnedt btn-secondary btn-sm float-right edit';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode('Edit'));
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+
+// Edit item
+function editItem(e){
+  if(e.target.classList.contains('edit')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      
+    }
+  }
+}
